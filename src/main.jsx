712d0117748b6556f563c3c69fab0d8f01263e91
Emmanuel Faniyi <Faniyiemmanuel2018@gmail.com>
2025-12-29 11:15:46 +0100

    co

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/bootstrap/dist/js/bootstrap.js";
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+// import "../node_modules/bootstrap/dist/js/bootstrap.js";
+// import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 // import "./index.css";
